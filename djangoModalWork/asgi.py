78457533d7edcdc72f4c31eda9a3fd874d48a72b
IllyaShparuk <illya.shparuk@knu.ua>
2024-05-31 10:27:40 +0300

    migrations

--- djangoModalWork/asgi.py
+++ djangoModalWork/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_gallery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoModalWork.settings')
 
 application = get_asgi_application()
--- djangoModalWork/wsgi.py
+++ djangoModalWork/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_gallery.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoModalWork.settings')
 
 application = get_wsgi_application()
