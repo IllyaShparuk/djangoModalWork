93fd14cc684b8c333c0b34e10bedf242991fe3f8
IllyaShparuk <illya.shparuk@knu.ua>
2024-05-31 09:25:37 +0300

    application setup

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    categories = models.ManyToManyField('Category')
+    created_date = models.DateField(auto_now_add=True)
+    age_limit = models.PositiveIntegerField(blank=True)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.category
